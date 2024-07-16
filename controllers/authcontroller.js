da6992c753563ca07daf1e82aec640f9351bce1a
Japneet kaur <166175929+JapneetK26@users.noreply.github.com>
2024-07-16 21:55:39 +0530

    Update authcontroller.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -47,7 +47,59 @@ module.exports.loginUser = async function (req, res) {
         }
     })
 }
+module.exports.forgotPassword = async (req, res) => {
+    const { email } = req.body;
 
+    try {
+        console.log(email);
+        const user = await userModel.findOne({ email });
+        if (!user) return res.status(404).send({ success: false, msg: "This email does not exist." });
+
+        const token = randomString.generate();
+        await userModel.updateOne({ email: email }, { $set: { token } });
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Password Reset',
+            html: `<p>Click on the link to reset your password: <a href="http://127.0.0.1:3000/reset-password?token=${token}">Reset Password</a></p>`
+        };
+
+        await transporter.sendMail(mailOptions);
+        res.status(200).send({ success: true, msg: "Please check your mail and reset your password." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, msg: error.message });
+    }
+};
+
+
+module.exports.resetPassword = async (req, res) => {
+    let { token, newPassword } = req.body;
+
+    try {
+        const user = await userModel.findOne({ token });
+        if (!user) return res.status(400).send({ success: false, msg: "Invalid or expired token." });
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(newPassword, salt);
+
+        const userNew=await userModel.findOneAndUpdate({ token }, { $set: { password: hash, token: null } },{new:true});
+
+        res.status(200).send({ success: true, msg: "Password has been reset successfully.",data:userNew });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, msg: error.message });
+    }
+};
 module.exports.logout = function (req, res) {
     res.cookie("token", "");
     res.redirect("/");
